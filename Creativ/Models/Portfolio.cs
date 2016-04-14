20631672731f5ec539fef1a85167ce743fe67a5c
Pito289 <Juan.M.Davila.Jr@hotmail.com>
2016-04-14 12:11:53 -0500

    Initial commit

---
+++ Creativ/Models/Portfolio.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace Creativ.Models
+{
+    [Table("Portfolio")]
+    public class Portfolio
+    {
+        [Key]       
+        public int PorfolioID { get; set; }
+        [Required]
+        public string PorfolioName { get; set; }
+        public ProjectType PorfolioType { get; set; }
+        public string PorfolioOwner { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime PublishDate { get; set; }
+
+    }
+
+   
+
+    public class ProjectType
+    {
+        public int ProjectItems { get; set; }
+        public string Type { get; set; }
+    }
+
+    enum Type
+    {
+        Drawing,
+        Photo,
+        Code,
+        Animation,
+        Writing
+
+    }
+
+   public class PortfolioDBContext : DbContext
+    {
+        public DbSet<Portfolio> Portfolios { get; set; }
+    }
+}
---
+++ Creativ/Models/Project.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace Creativ.Models
+{
+    public class Project
+    {
+
+        [Key]
+        public int ProjectID { get; set; }
+        [Required]    
+        public string ProjectName { get; set; }
+        public ProjectType ProjectType { get; set; }
+        public string ProjectOwner { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+
+    public class ProjectDBContext : DbContext
+    {
+        public DbSet<Project> Projects { get; set; }
+    }
+}
